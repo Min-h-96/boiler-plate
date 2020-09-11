2780635d86c787a6f0565a60a7f66a8a87b782f7
Min-h-96 <asdf6256@gmail.com>
2020-09-11 18:08:35 +0900

    finish #8

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const bodyParser = require("body-parser");
+const { User } = require("./models/user");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
@@ -14,7 +20,17 @@ mongoose
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World! test nodemon");
+});
+
+app.post("/register", (req, res) => {
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ succes: false, err });
+    return res.status(200).json({
+      succes: true,
+    });
+  });
 });
 
 app.listen(port, () => {
