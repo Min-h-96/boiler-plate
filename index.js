d4b39ccfb8c655a8705bc7d6d669759e342962c2
Min-h-96 <asdf6256@gmail.com>
2020-09-11 17:28:00 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const mongoose = require("mongoose");
+mongoose
+  .connect("mongodb+srv://minhyuk:chl153249@boiler-plate.qk2zz.mongodb.net/boiler-plate?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("MongoDB connected..."))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
