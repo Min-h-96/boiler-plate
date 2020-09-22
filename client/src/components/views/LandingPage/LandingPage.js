88e830604eadcaf524c901c4773d73b53a2921c3
Min-h-96 <asdf6256@gmail.com>
2020-09-22 16:55:12 +0900

    #32 로그아웃 기능

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,6 +1,17 @@
 import React from "react";
+import axios from "axios";
 
-function LandingPage() {
+function LandingPage(props) {
+  const onClickHandler = () => {
+    axios.get("/api/users/logout").then((response) => {
+      if (response.data.success) {
+        props.history.push("/login");
+      } else {
+        alert("로그아웃 하는데 실패 했습니다.");
+      }
+    });
+  };
+
   return (
     <div
       style={{
@@ -13,7 +24,7 @@ function LandingPage() {
     >
       <h2>시작 페이지</h2>
 
-      <button>로그아웃</button>
+      <button onClick={onClickHandler}>로그아웃</button>
     </div>
   );
 }
